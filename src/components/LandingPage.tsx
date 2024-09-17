761e2b2aa5c280b96550353e91c4890fe295e086
Nebalus <56486387+Nebalus@users.noreply.github.com>
2024-09-17 15:48:49 +0200

    master: .

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -23,7 +23,7 @@ To read more about using these font, please visit the Next.js documentation:
 - App Directory: https://nextjs.org/docs/app/building-your-application/optimizing/fonts
 - Pages Directory: https://nextjs.org/docs/pages/building-your-application/optimizing/fonts
 **/
-import { Card, CardContent } from "@assets/components/ui/card.tsx"
+import { Card, CardContent } from "@assets/components/shadcnui/card.tsx"
 // import { Input } from "@assets/components/ui/input.tsx"
 // import { Textarea } from "@assets/components/ui/textarea.tsx"
 // import { Button } from "@assets/components/ui/button.tsx"
