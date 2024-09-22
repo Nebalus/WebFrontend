21e3000595a8cc731f80607c4ac69b8059d47417
nebalus <coding@nebalus.dev>
2024-09-22 23:26:22 +0200

    .

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -129,8 +129,6 @@ export default function LandingPage() {
                 </div>
               </div>
             </section>
-            <br/>
-            {/*
             <section id="skills" className="w-full py-12 md:py-24 lg:py-32 bg-muted">
               <div className="container px-4 md:px-6">
                 <div className="flex flex-col items-center justify-center space-y-4 text-center">
