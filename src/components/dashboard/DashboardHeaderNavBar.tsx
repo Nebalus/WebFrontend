b09ad01de4c2223353af4184fa236621c96de752
Nebalus <56486387+Nebalus@users.noreply.github.com>
2024-09-24 15:49:32 +0200

    master: .

--- src/components/dashboard/DashboardHeaderNavBar.tsx
+++ src/components/dashboard/DashboardHeaderNavBar.tsx
@@ -1,7 +1,43 @@
+
+import {Button} from "@assets/components/shadcnui/button.tsx";
+import {NavLink} from "react-router-dom";
+import {Avatar, AvatarFallback} from "@radix-ui/react-avatar";
+import ThemeToggle from "@/components/dashboard/ThemeToggle.tsx";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@assets/components/shadcnui/dropdown-menu.tsx";
+
 export default function DashboardHeaderNavBar() {
     return (
         <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-
+            <div className="flex gap-2 ml-auto">
+                <ThemeToggle />
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="secondary" size="lg" className="rounded-full">
+                            <Avatar>
+                                <AvatarFallback>TE</AvatarFallback>
+                            </Avatar>
+                            <span className="sr-only">Toggle user menu</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator/>
+                        <DropdownMenuItem className="cursor-pointer">Settings</DropdownMenuItem>
+                        <DropdownMenuItem className="cursor-pointer">Support</DropdownMenuItem>
+                        <DropdownMenuSeparator/>
+                        <NavLink to="/logout">
+                            <DropdownMenuItem className="cursor-pointer">Logout</DropdownMenuItem>
+                        </NavLink>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </header>
-    );
+);
 }
--- src/components/dashboard/DashboardRootLayout.tsx
+++ src/components/dashboard/DashboardRootLayout.tsx
@@ -1,13 +1,34 @@
 import DashboardNavBar from "@/components/dashboard/DashboardNavBar.tsx";
 import DashboardHeaderNavBar from "@/components/dashboard/DashboardHeaderNavBar.tsx";
+import {Outlet} from "react-router-dom";
+import {Toaster, toast} from "sonner";
+import {Button} from "@assets/components/shadcnui/button.tsx";
 
 export default function DashboardRootLayout() {
     return (
-        <>
-            <div className="grid h-full w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] fixed">
-                <DashboardNavBar/>
-                <DashboardHeaderNavBar />
+        <div className="grid h-full w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] fixed">
+            <DashboardNavBar/>
+            <div className="flex flex-col">
+                <DashboardHeaderNavBar/>
+                <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+                    <Outlet/>
+                    <Button
+                        variant="outline"
+                        onClick={() =>
+                            toast("Event has been created", {
+                                description: "Sunday, December 03, 2023 at 9:00 AM",
+                                action: {
+                                    label: "Undo",
+                                    onClick: () => console.log("Undo"),
+                                },
+                            })
+                        }
+                    >
+                        Show Toast
+                    </Button>
+                </main>
+                <Toaster/>
             </div>
-        </>
+        </div>
     );
 }
