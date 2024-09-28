c39534fdad5d640d9c4fa8ef0462ba9fc1b34d48
nebalus <coding@nebalus.dev>
2024-09-28 18:09:28 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
-import { defineConfig } from 'vite'
+import path from 'path';
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-
   console.log(`CURRENT COMMAND: ${command}`);
   console.log(`CURRENT MODE: ${mode}`);
 
+  const env = loadEnv(mode, process.cwd(), 'APP_');
+
   return {
-    plugins: [react()],
+    plugins: [
+        react()
+    ],
+    define: {
+        'import.meta.env.APP_BACKEND_API_DOMAIN': JSON.stringify(env.APP_BACKEND_API_DOMAIN),
+        'import.meta.env.APP_BACKEND_API_PROTOCOL': JSON.stringify(env.APP_BACKEND_API_PROTOCOL),
+        'import.meta.env.APP_BACKEND_API_PORT': JSON.stringify(env.APP_BACKEND_API_PORT),
+    },
     resolve: {
         alias: {
-          "@": "/src",
-          "@assets": "/assets",
+          "@": path.resolve(__dirname, "./src"),
+          "@assets": path.resolve(__dirname, "./assets"),
         },
     }
   }
