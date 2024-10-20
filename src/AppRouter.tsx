1e1db8b843ba162e32a2687d8995599fd03c1a6a
Nebalus <56486387+Nebalus@users.noreply.github.com>
2024-10-20 19:55:40 +0000

    .

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -30,7 +30,7 @@ export const appRouter = createBrowserRouter([
     {
         path: APP_DASHBOARD_PATH,
         element: <DashboardRootLayout />,
-        loader: protectedLoader,
+        // loader: protectedLoader,
         children: [
             {
                 path: "",
