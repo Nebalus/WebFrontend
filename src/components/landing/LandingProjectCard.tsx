3c49a56540dbb7d21d0b08996af8d720b7344da7
nebalus <coding@nebalus.dev>
2025-02-11 17:51:49 +0100

    Coderefractor

--- src/components/landing/LandingProjectCard.tsx
+++ src/components/landing/LandingProjectCard.tsx
@@ -14,31 +14,31 @@ export interface ProjectCardProps {
 export default function LandingProjectCard({ pic_link, pic_alt, title, description, github_link, work_and_progress}: ProjectCardProps) {
     return (
         <div className="rounded-2xl bg-gray-900 shadow-2xl shadow-gray-700 border-solid border-2 border-gray-950 group hover:shadow-violet-950 transition-all duration-300">
-            <div className="flex flex-col h-full">
-                <AspectRatio ratio={16 / 9} className="overflow-hidden">
-                    <img
-                        draggable="false"
-                        src={pic_link}
-                        alt={pic_alt}
-                        className="h-full w-full object-cover object-center group-hover:rounded-t-2xl group-hover:scale-110 transition-all duration-300"
-                    />
-                </AspectRatio>
-                <div className="flex flex-col gap-3 p-4 h-full">
-                    <h3 className="text-xl font-bold">{title}</h3>
-                    <p className="text-muted-foreground">{description}</p>
-                    <div className="flex flex-row last mt-auto">
-                        {github_link && (
-                            <ExternalLink to={github_link}>
+            <ExternalLink to={github_link}>
+                <div className="flex flex-col h-full">
+                    <AspectRatio ratio={16 / 9} className="overflow-hidden">
+                        <img
+                            draggable="false"
+                            src={pic_link}
+                            alt={pic_alt}
+                            className="h-full w-full object-cover object-center group-hover:rounded-t-2xl group-hover:scale-110 transition-all duration-300"
+                        />
+                    </AspectRatio>
+                    <div className="flex flex-col gap-3 p-4 h-full">
+                        <h3 className="text-xl font-bold">{title}</h3>
+                        <p className="text-muted-foreground">{description}</p>
+                        <div className="flex flex-row last mt-auto">
+                            {github_link && (
                                 <Github />
-                            </ExternalLink>
-                        )}
+                            )}
 
-                        {work_and_progress && <p className="flex flex-row ">
-                            <Pickaxe /> WIP
-                        </p>}
+                            {work_and_progress && <p className="flex flex-row ">
+                                <Pickaxe /> WIP
+                            </p>}
+                        </div>
                     </div>
                 </div>
-            </div>
+            </ExternalLink>
         </div>
     )
 }
